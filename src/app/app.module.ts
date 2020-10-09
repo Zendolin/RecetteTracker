f825cf0124949049a6354756762b25ad6aaed643
Zendolin <josh.valmy@gmail.com>
2020-10-09 21:41:19 +0200

    design/feat add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,28 @@ import { AppComponent } from './app.component';
 import { RecipeCardComponent } from './recipe-card/recipe-card.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeService } from './services/recipe.service';
+import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { RecipeFormComponent } from './recipe-form/recipe-form.component';
+
+
+const appRoutes: Routes = [
+  {path: "add", component: RecipeFormComponent},
+  {path: "", component: RecipeListComponent},
+  {path: "**", redirectTo: ""}  
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     RecipeCardComponent,
-    RecipeListComponent
+    RecipeListComponent,
+    HeaderComponent,
+    RecipeFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     RecipeService
