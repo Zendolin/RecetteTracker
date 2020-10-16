c8d75a5a403b677f216ff8c1b77005a86ba2a68c
Zendolin <josh.valmy@gmail.com>
2020-10-16 17:38:13 +0200

    feat: state isAuth

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { firebase } from "@firebase/app";
+import "@firebase/auth"
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  isAuth: boolean
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    firebase.auth().onAuthStateChanged(
+      (user) => {
+        if(user){
+          this.isAuth = true
+        }
+        else{
+          this.isAuth = false
+        }
+      }
+    )
+  }
+
+  onSignOut(){
+    this.authService.signOut()
   }
 
 }
