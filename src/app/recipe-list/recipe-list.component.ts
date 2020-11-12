480c63c9c1e1fb43fdee766f0932cb20287bf7f2
Zendolin <josh.valmy@gmail.com>
2020-11-12 19:02:41 +0100

    feat: simple search

--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Recipe } from '../models/recipe.model';
@@ -11,24 +12,95 @@ import { RecipeService } from '../services/recipe.service';
 })
 export class RecipeListComponent implements OnInit {
 
+  allRecipes: Recipe[]
   recipes: Recipe[]
   recipesSubscription: Subscription
 
-  constructor(private recipesService: RecipeService) { 
+  searchForm: FormGroup
+
+  constructor(private recipesService: RecipeService,
+              private formBuilder: FormBuilder) { 
     
   }
 
   ngOnInit(): void {
     this.recipesSubscription = this.recipesService.recipesSubject.subscribe(
       (recipes: Recipe[]) => {
-        this.recipes = recipes.filter(recipe => recipe)
+        this.allRecipes = recipes.filter(recipe => recipe)
+        this.recipes = this.allRecipes
       }
     )
     this.recipesService.emitRecipes()
+    this.initForm()
   }
 
   ngOnDestroy(){
     this.recipesSubscription.unsubscribe()
+  }
+
+  initForm(){
+    this.searchForm = this.formBuilder.group({
+      search: ["", Validators.required]
+    })
+  }
+
+  onSearch(){
+    const formValue = this.searchForm.value
+    const search = formValue["search"]
+
+    this.searchForm.setValue({"search" :this.clean(search)})
+
+    this.recipes = this.allRecipes.filter((recipe) => {
+      const uglifySearch = this.uglify(search)
+      const titleRecipe = this.uglify(recipe.name)
+
+
+      return(uglifySearch === titleRecipe || this.includesAll(titleRecipe, uglifySearch))
+    })
+  }
+
+  includesAll(s1: string, s2: string): boolean{
+    let isAllInclude = true
+    let s2Array = s2.split(" ")
+
+    for(let s2Item of s2Array){
+      if(!s1.includes(s2Item)){
+        return false
+      }
+    }
+
+    return isAllInclude
+  }
+
+  clean(search: string): string{
+    let cleanSearch = search
+
+    cleanSearch = cleanSearch.replace(/\s{2,}/g, " ")
+
+    /* Espace à la fin/début */
+    .replace(/^\s/, "").replace(/\s$/, "")
+
+
+    return cleanSearch
+  }
+
+  uglify(search: string): string{
+    let uglifyString = search
+
+    /* Espace en trop */
+    uglifyString = uglifyString.replace(/\s{2,}/g, " ")
+
+    /* Espace à la fin/début */
+    .replace(/^\s/, "").replace(/\s$/, "")
+
+    /* Accent */
+    .normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+
+    /* Majuscule */
+    .toLowerCase()
+
+
+    return uglifyString
   }
 
 
